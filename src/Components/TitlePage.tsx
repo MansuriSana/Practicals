69bf6de8a41eade959b7c787a5dd13ac26241815
JayeshA <jayesh@kvsocial.com>
2023-12-08 13:41:51 +0530

    do seprate css for desktop and mobile example.

---
+++ src/Components/TitlePage.tsx
@@ -0,0 +1,13 @@
+import "./titlePageDesktop.scss"
+import "./titlePageMobile.scss"
+
+export const TitlePage = () => {
+    return (
+        <div className="main-container">
+            <div className="div-one">div 1</div>
+            <div className="div-two">div 2</div>
+            <div className="div-three">div 3</div>
+            <div className="div-four">div 4</div>
+        </div>
+    )
+}
